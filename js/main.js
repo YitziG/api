214d157f513ad132750045580bd331efa185cc8b
Yitzi Ginzberg <emesdg@gmail.com>
2019-11-20 17:45:01 +0200

    fingerprint code

--- js/main.js
+++ js/main.js
@@ -70,6 +70,51 @@ jQuery(function() {
 	}
 });
 
+console.log(new AudioContext());
+let freq_data = [];
+
+// Create nodes
+const ctx = new AudioContext(); // AudioContext Object
+const oscillator = ctx.createOscillator(); // OscillatorNode
+const analyser = ctx.createAnalyser(); // AnalyserNode
+const gain = ctx.createGain(); // GainNode
+const scriptProcessor = ctx.createScriptProcessor(4096, 1, 1); // ScriptProcessorNode
+
+// Disable volume
+gain.gain.value = 0;
+
+// Connect oscillator output (OscillatorNode) to analyser input
+oscillator.connect(analyser);
+// Connect analyser output (AnalyserNode) to scriptProcessor input
+analyser.connect(scriptProcessor);
+// Connect scriptProcessor output (ScriptProcessorNode) to gain input
+scriptProcessor.connect(gain);
+// Connect gain output (GainNode) to AudioContext destination
+gain.connect(ctx.destination);
+
+scriptProcessor.onaudioprocess = function(bins) {
+	// The number of (frequency) data values
+	bins = new Float32Array(analyser.frequencyBinCount);
+	// Fill the Float32Array array of these based on values
+	analyser.getFloatFrequencyData(bins);
+
+	// Copy frequency data to 'freq_data' array
+	for (var i = 0; i < bins.length; i = i + 1) {
+		freq_data.push(bins[i]);
+	}
+
+	// Disconnect the nodes from each other
+	analyser.disconnect();
+	scriptProcessor.disconnect();
+	gain.disconnect();
+
+	// Log output of frequency data
+	console.log(freq_data);
+};
+
+// Start playing tone
+oscillator.start(0);
+
 function getClosestHeader() {
 	var $links = $('.sidebar a'),
 	top = window.scrollY,
